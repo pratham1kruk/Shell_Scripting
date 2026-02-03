05dcb2d8cf1fe3973cef23a4cf4d0f63182e785b
Ubuntu <ubuntu@ip-172-31-21-116.eu-north-1.compute.internal>
2026-02-03 06:38:42 +0000

    fixes

--- testweb.sh
+++ testweb.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 # Update system packages
-sudo yum update -y
+sudo apt update -y
 
-# Install Apache web server (httpd)
-sudo yum install -y httpd
+# Install Apache web server
+sudo apt install -y apache2
 
 # Start Apache service
-sudo systemctl start httpd
+sudo systemctl start apache2
 
 # Enable Apache to start on boot
-sudo systemctl enable httpd
+sudo systemctl enable apache2
 
 # Create a simple HTML file to verify the web server is running
 # Includes the dynamic hostname
-echo "<html><h1>Welcome to Apache Web Server on Amazon Linux - $(hostname)!</h1></html>" | sudo tee /var/www/html/index.html
+echo "<html><h1>Welcome to Apache Web Server on Ubuntu - $(hostname)!</h1></html>" | sudo tee /var/www/html/index.html
+
